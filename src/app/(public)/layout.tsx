6c79a3e5fa0a06354c0e2c8a4c0f44ada6b69197
Rohit Grover <developer@mytalente.com>
2024-06-28 11:18:07 +0530

    ahifted root page and layout inside (public) folder

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NotistackProvider from "@/utils/Notistack/NotiStackProvider";
 import "react-international-phone/style.css";
-import "./globals.css";
+import "../globals.css";
 import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
