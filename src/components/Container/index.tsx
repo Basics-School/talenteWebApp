a30acfd758ea5836dd729d43578323f6843cb3a8
Rohit Grover <developer@mytalente.com>
2024-06-29 13:58:43 +0530

    container fixes

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MUIContainer from "@mui/material/Container";
+
+interface IProps {
+  children: React.ReactNode;
+}
+
+const Container = ({ children }: IProps) => {
+  return <MUIContainer className="max-w-[1366px]">{children}</MUIContainer>;
+};
+
+export default Container;
