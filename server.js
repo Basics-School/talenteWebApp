c6884873c029fb32fefe622c83eadf4a47d3223a
Rohit Grover <developer@mytalente.com>
2024-07-01 20:54:32 +0530

    updated serverjs

--- server.js
+++ server.js
@@ -1,26 +1,33 @@
-const { createServer } = require("http");
+const express = require("express");
 const next = require("next");
 
-const port = 8080;
 const dev = process.env.NODE_ENV !== "production";
+const port = process.env.PORT || 3000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = new URL(req.url, "http://w.w");
-    const { pathname, query } = parsedUrl;
+app
+  .prepare()
+  .then(() => {
+    const server = express();
 
-    if (pathname === "/a") {
-      app.render(req, res, "/a", query);
-    } else if (pathname === "/b") {
-      app.render(req, res, "/b", query);
-    } else {
-      handle(req, res, parsedUrl);
-    }
-  }).listen(port, (err) => {
-    if (err) throw err;
+    server.get("/p/:id", (req, res) => {
+      const actualPage = "/post";
+      const queryParams = { title: req.params.id };
+      app.render(req, res, actualPage, queryParams);
+    });
+
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(port, (err) => {
+      if (err) throw err;
+      //console.log('> Ready on http://localhost:' + port)
+    });
+  })
+  .catch((ex) => {
     // eslint-disable-next-line no-console
-    console.log(`> Ready on http://localhost:${port}`);
+    console.error(ex.stack);
+    process.exit(1);
   });
-});
