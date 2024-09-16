7a4dfffd66c8a1034f8577f24e22bd7291de2038
mamorshedalam <ma.morshedalampro@gmail.com>
2024-09-16 11:06:09 +0800

    image height bug fixed

--- src/common/ImageDiv/index.tsx
+++ src/common/ImageDiv/index.tsx
@@ -8,7 +8,7 @@ type PropsType = {
 export default function ImageDiv({ ImgSrc, ImgAlt }: PropsType) {
   const content = (
     <div className="md:w-80 w-60 md:h-80 h-60 md:border-[1.6rem] sm:border-[1rem] border-8 border-[#ffb54d] rounded-full overflow-hidden sm:my-10 my-5 mx-auot">
-      <Image draggable="false" src={ImgSrc} alt={ImgAlt} />
+      <Image draggable="false" src={ImgSrc} alt={ImgAlt} className="w-full h-full object-cover" />
     </div>
   );
 
