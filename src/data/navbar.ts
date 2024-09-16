08b67752b61af28fc4c0784f7eafe0dd820a725b
mamorshedalam <ma.morshedalampro@gmail.com>
2024-09-16 12:28:49 +0800

    changed ask-tash to tash

--- src/data/navbar.ts
+++ src/data/navbar.ts
@@ -1,5 +1,5 @@
 export const usefulLinks = [
-  { label: "Ask Tasha", path: "/ask-tasha" },
+  { label: "Tasha", path: "/tasha" },
   { label: "Career Hub", path: "/", url: "https://community.mytalente.ai/" },
   { label: "Subscription", path: "/pricing" },
   { label: "Company", path: "/about-us" },
